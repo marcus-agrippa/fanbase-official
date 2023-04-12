b090fb75956316d25eb40efcc8a04d851c5a4af8
marcus-agrippa <benjamincowan1@gmail.com>
2023-04-13 09:50:20 +1000

    update text size on about page for smaller devices

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1 className='text-6xl mb-4'>Fanbase Official</h1>
-      <p className='mb-4 text-2xl font-light'>An app to help you track all fixtures, results, standings, data, news and social media related to your favourite football club.<br></br> This app is in constant development and may have errors or inconsistencies, which will be improved over time.<br></br> It was made by a football fan for football fans, so if you have any suggestions please get in contact.</p>
+      <h1 className='text-2xl mb-4 md:text-5xl text-white'>ClubFanBase</h1>
+      <p className='mb-4 text-lg font-light md:text-2xl'>An app to help you track all fixtures, results, standings, data, news and social media related to your favourite football club.<br></br> This app is in constant development and may have errors or inconsistencies, which will be improved over time.<br></br> It was made by a football fan for football fans, so if you have any suggestions please get in contact.</p>
       <p className='text-lg text-gray-400'>Version <span className='text-white'>1.0.0</span></p>
       <p className='text-lg text-gray-400'>Created by <span className='text-white'><a href="https://github.com/marcus-agrippa">@marcus-arippa</a></span></p>
     </div>
