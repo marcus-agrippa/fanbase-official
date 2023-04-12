b0302e94116175c0c2ff1f517b5b04a6d5a7e927
marcus-agrippa <benjamincowan1@gmail.com>
2023-04-12 21:47:21 +1000

    add netlify.toml file, change .gitignore file and add buymeacoffee link to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SiBuymeacoffee } from 'react-icons/si'
 
 const Footer = () => {
   const footerYear = new Date().getFullYear()
@@ -7,6 +8,7 @@ const Footer = () => {
     <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
       <div>
         <p>Copyright &copy; {footerYear} All rights reserved</p>
+       <a href="https://www.buymeacoffee.com/clubfanbase"><SiBuymeacoffee className='inline pr-2 text-5xl'/> Buy Me A Coffee</a> 
       </div>
     </footer>
   )
