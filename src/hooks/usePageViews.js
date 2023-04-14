a486d875f28293b2ba7245ce518657982b91a05a
marcus-agrippa <benjamincowan1@gmail.com>
2023-04-14 11:17:51 +1000

    updae to GA4

--- src/hooks/usePageViews.js
+++ src/hooks/usePageViews.js
@@ -1,16 +1,14 @@
 // usePageViews.js
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from "react-ga4";
+import ReactGA from 'react-ga4'; // Change the import here
 
 export const usePageViews = () => {
   const location = useLocation();
 
   useEffect(() => {
     try {
-      console.log('Sending pageview:', location.pathname + location.search);
-      ReactGA.set({ page: location.pathname + location.search });
-      ReactGA.pageview(location.pathname + location.search);
+      ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
     } catch (error) {
       console.error('Google Analytics error:', error);
     }
