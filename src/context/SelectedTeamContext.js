7d1119cd9e7d550389cb0c23cf05d926a0528717
marcus-agrippa <benjamincowan1@gmail.com>
2023-04-22 11:18:09 +1000

    initialise league and team state on initial mount

--- src/context/SelectedTeamContext.js
+++ src/context/SelectedTeamContext.js
@@ -3,7 +3,16 @@ import { createContext, useState } from 'react';
 const SelectedTeamContext = createContext();
 
 export const SelectedTeamProvider = ({ children }) => {
-  const [selectedLeague, setSelectedLeague] = useState('');
+  const leagues = [
+    { id: 39, name: "Premier League" },
+    { id: 140, name: "La Liga" },
+    { id: 135, name: "Serie A" },
+    { id: 61, name: "Ligue 1" },
+    { id: 78, name: "Bundesliga" },
+    { id: 179, name: "SPL" },
+  ];
+
+  const [selectedLeague, setSelectedLeague] = useState(leagues[0].id);
   const [selectedTeam, setSelectedTeam] = useState('');
 
   return (
