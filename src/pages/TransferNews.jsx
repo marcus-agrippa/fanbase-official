2535f441939331a869dc91c0433b6f1e0ff36b47
marcus-agrippa <benjamincowan1@gmail.com>
2023-07-01 18:14:52 +1000

    update transfer news page

--- src/pages/TransferNews.jsx
+++ src/pages/TransferNews.jsx
@@ -4,6 +4,9 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 const TransferNews = () => {
   return (
     <div className='w-[90vw]'>
+      <h1 className="text-3xl flex flex-col font-bold text-white text-center my-10">
+        Transfer News
+      </h1>
       <div className="twitter-feed grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-4 mt-4">
         <div className='bg-gray-900 p-12 rounded twitter-feed shadow flex flex-col items-center'>
           <h1 className="text-3xl flex flex-col font-bold text-white text-center my-10">
