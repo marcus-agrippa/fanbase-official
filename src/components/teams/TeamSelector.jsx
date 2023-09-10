8b3ac6956f5c3fbbe7616dc28c710d938b6f85c1
marcus-agrippa <benjamincowan1@gmail.com>
2023-09-10 14:14:09 +1000

    sort team selector alphabetically

--- src/components/teams/TeamSelector.jsx
+++ src/components/teams/TeamSelector.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const TeamSelector = ({ teams, selectedTeam, handleTeamSelect }) => (
-  <select value={selectedTeam} onChange={handleTeamSelect} className='bg-white text-black text-sm md:text-base bg-white p-1 rounded-md'>
-    <option value="">-- Select a team --</option>
-    {teams.map(team => (
-      <option key={team.id} value={team.id}>
-        {team.name}
-    </option>
-    ))}
-  </select>
-);
+const TeamSelector = ({ teams, selectedTeam, handleTeamSelect }) => {
+  const sortedTeams = [...teams].sort((a, b) => a.name.localeCompare(b.name));
+
+  return (
+    <select
+      value={selectedTeam}
+      onChange={handleTeamSelect}
+      className='bg-white text-black text-sm md:text-base bg-white p-1 rounded-md'
+    >
+      <option value="">-- Select a team --</option>
+      {sortedTeams.map((team) => (
+        <option key={team.id} value={team.id}>
+          {team.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default TeamSelector;
